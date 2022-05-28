d9c77a32e9b55089016faee669884c738e941165
l-busca <ludovic.busca@icloud.com>
2022-05-28 19:29:10 +0200

    Script de la bdd mis à jour avec un petit jeu de test pour les clients

--- parky.sql
+++ parky.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 27 mai 2022 à 17:04
+-- Généré le : sam. 28 mai 2022 à 17:27
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -50,14 +50,19 @@ CREATE TABLE IF NOT EXISTS `client` (
   `carte` char(64) NOT NULL,
   `mdp` char(64) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `client`
 --
 
 INSERT INTO `client` (`id`, `nom`, `prenom`, `adresse`, `numero`, `carte`, `mdp`) VALUES
-(1, 'Busca', 'Ludovic', 'Rue de l\'abbé gridel', '0678986532', '110812f67fa1e1f0117f6f3d70241c1a42a7b07711a93c2477cc516d9042f9db', '50f8309afb642ed4234bb5036760fa61d0f026c037a1800d0a53c7a601a7a521');
+(1, 'Busca', 'Ludovic', 'Rue de l\'abbé gridel', '0678986532', '110812f67fa1e1f0117f6f3d70241c1a42a7b07711a93c2477cc516d9042f9db', '50f8309afb642ed4234bb5036760fa61d0f026c037a1800d0a53c7a601a7a521'),
+(2, 'Busca', 'Ludovic', 'adresse de test', '0674997203', 'eda71b82d84a90e3716b3dac2ba561b938755cd371e4733cbebc51018d1c2c85', '6f28e903b309e7f3eac92fc74e7417f23ec330556259d8ca0c880771dd86172b'),
+(3, 'Simon', 'Jordan', 'adresse de test', '0488956231', 'eda71b82d84a90e3716b3dac2ba561b938755cd371e4733cbebc51018d1c2c85', '9248dbb4d94adfc7455990248486f1f9d8ded09ba398ee72e24071d3652f89e5'),
+(4, 'Lion', 'Boulon', 'adresse de test', '0789542321', 'eda71b82d84a90e3716b3dac2ba561b938755cd371e4733cbebc51018d1c2c85', '4f682b71153ffa91e608445d7ea1257e2076d0d95eab6336cd1aa94b49680f11'),
+(5, 'Laurent', 'Yugi', 'adresse de test', '0714589632', 'eda71b82d84a90e3716b3dac2ba561b938755cd371e4733cbebc51018d1c2c85', '4f682b71153ffa91e608445d7ea1257e2076d0d95eab6336cd1aa94b49680f11'),
+(6, 'Oui', 'Non', 'adresse de test', '0674882694', 'eda71b82d84a90e3716b3dac2ba561b938755cd371e4733cbebc51018d1c2c85', '4f682b71153ffa91e608445d7ea1257e2076d0d95eab6336cd1aa94b49680f11');
 
 -- --------------------------------------------------------
 
@@ -111,6 +116,8 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   `borne` int(11) NOT NULL,
   `possession` int(11) NOT NULL,
   `prix` double NOT NULL,
+  `temps` int(11) NOT NULL,
+  `date` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
