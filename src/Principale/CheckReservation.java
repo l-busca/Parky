ae2353c77b151382caac55bc4faa39d8b68edc1e
l-busca <ludovic.busca@icloud.com>
2022-05-29 22:06:19 +0200

    Checker reservation

--- src/Principale/CheckReservation.java
+++ src/Principale/CheckReservation.java
@@ -1,5 +1,29 @@
 package Principale;
 
-public class CheckReservation {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class CheckReservation {
+	private Timer timer;
+	
+	//Vérifie tout ce qu'il faut dans la base de donnée
+	public CheckReservation() {
+		
+	}
+	
+	public void init() {
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
+		  @Override
+		  public void run() {
+		    //Check les trucs de la bdd
+		  }
+		}, 0, 2000);//chaque 2 secondes
+
+		
+	}
+	
+	public void stop() {
+		timer.cancel();//arrete le check
+	}
 }
